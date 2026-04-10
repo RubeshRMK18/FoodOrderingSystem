c3b05c6a01b6bac3793c903b5824bcd0c1e54d4f
harisakthi301104 <harisakthi301104@gmail.com>
2026-04-10 09:48:13 +0530

    Delete DTO directory

--- DTO/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FoodOrderingSystem.DTO
-{
-    public class LoginDTO
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
