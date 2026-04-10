8c8df0faea0e6d180852e00520ffcd54bb23c03e
harisakthi301104 <harisakthi301104@gmail.com>
2026-04-10 09:48:58 +0530

    Delete Model directory

--- Model/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FoodOrderingSystem.Model
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string HashPassword { get; set; }
-    }
-}
